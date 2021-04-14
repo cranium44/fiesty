e2f50adddcbc0eb43ec6c85b55ce2c04cfa539e7
Adabe Echaim <adabeechaim@gmail.com>
2021-04-14 18:36:06 +0100

    initial commit

---
+++ app/src/main/java/live/adabe/fiesty/BaseApplication.kt
@@ -0,0 +1,8 @@
+package live.adabe.fiesty
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication: Application() {
+}
---
+++ app/src/main/java/live/adabe/fiesty/ui/HomeViewModel.kt
@@ -0,0 +1,6 @@
+package live.adabe.fiesty.ui
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel: ViewModel() {
+}
