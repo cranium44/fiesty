fa34e3a182995b4a3aec9d7a605de3e98d0a42f4
Adabe Echaim <adabeechaim@gmail.com>
2021-04-26 15:54:24 +0100

    implement room Api

---
+++ app/src/main/java/live/adabe/fiesty/models/network/room/RoomRequest.kt
@@ -0,0 +1,6 @@
+package live.adabe.fiesty.models.network.room
+
+data class RoomRequest(
+    val name: String,
+    val numberOfDevices: Int
+)
--- app/src/main/java/live/adabe/fiesty/network/RoomAPI.kt
+++ app/src/main/java/live/adabe/fiesty/network/RoomAPI.kt
@@ -1,4 +1,30 @@
 package live.adabe.fiesty.network
 
+import androidx.lifecycle.LiveData
+import live.adabe.fiesty.models.network.room.RoomRequest
+import live.adabe.fiesty.models.network.room.RoomResponse
+import retrofit2.http.*
+
 interface RoomAPI {
+    @POST(NetworkConstants.ROOM_CREATE_ENDPOINT)
+    suspend fun createRoom(
+        @Path("buildingId") buildingId: Int,
+        @Body roomRequest: RoomRequest
+    ): LiveData<RoomResponse>
+
+    @GET(NetworkConstants.ROOM_GET_ENDPOINT)
+    suspend fun getRoom(@Path("roomId") roomId: Int): LiveData<RoomResponse>
+
+    @GET(NetworkConstants.ROOM_GETALL_ENDPOINT)
+    suspend fun getAllRooms(@Path("buildingId") buildingId: Int): LiveData<List<RoomResponse>>
+
+    @PUT(NetworkConstants.ROOM_UPDATE_ENDPOINT)
+    suspend fun updateRoom(
+        @Path("roomId") roomId: Int,
+        @Path("buildingId") buildingId: Int,
+        @Body roomRequest: RoomRequest
+    ): LiveData<RoomResponse>
+
+    @DELETE(NetworkConstants.ROOM_DELETE_ENDPOINT)
+    suspend fun deleteRoom(@Path("roomId") roomId: Int)
 }
