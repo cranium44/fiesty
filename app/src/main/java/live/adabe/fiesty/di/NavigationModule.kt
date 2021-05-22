7460c5b28674f38a1b6f3ccc6f80f17234d7beb1
Adabe Echaim <adabeechaim@gmail.com>
2021-05-22 16:37:57 +0100

    fix navigation service injection

--- app/src/main/java/live/adabe/fiesty/di/NavigationModule.kt
+++ app/src/main/java/live/adabe/fiesty/di/NavigationModule.kt
@@ -8,16 +8,19 @@ import live.adabe.fiesty.navigation.NavigationService
 import live.adabe.fiesty.navigation.NavigationServiceImpl
 import ru.terrakok.cicerone.Cicerone
 import ru.terrakok.cicerone.Router
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 class NavigationModule {
     @Provides
+    @Singleton
     fun provideCicerone(): Cicerone<Router> {
         return Cicerone.create()
     }
 
     @Provides
+    @Singleton
     fun provideNavigationService(cicerone: Cicerone<Router>): NavigationService {
         return NavigationServiceImpl(cicerone)
     }
