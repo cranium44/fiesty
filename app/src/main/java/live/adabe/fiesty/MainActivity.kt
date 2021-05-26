a833d13c9a5548540dc1a2413db1b325a877e599
Adabe Echaim <adabeechaim@gmail.com>
2021-05-26 21:35:52 +0100

    :bug: fix file permissions

--- app/src/main/java/live/adabe/fiesty/MainActivity.kt
+++ app/src/main/java/live/adabe/fiesty/MainActivity.kt
@@ -1,7 +1,12 @@
 package live.adabe.fiesty
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
 import dagger.hilt.android.AndroidEntryPoint
 import live.adabe.fiesty.databinding.ActivityMainBinding
@@ -32,6 +37,7 @@ class MainActivity : AppCompatActivity() {
         } else {
             navigationService.openHomeScreen()
         }
+        requestPermissions()
         setContentView(binding.root)
     }
 
@@ -44,4 +50,48 @@ class MainActivity : AppCompatActivity() {
         super.onResumeFragments()
         navigationService.attachToActivity(this)
     }
+
+    private fun requestPermissions(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            when {
+                ContextCompat.checkSelfPermission(
+                    applicationContext,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    // You can use the API that requires the permission.
+                }
+                shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE) -> {
+                // In an educational UI, explain to the user why your app requires this
+                // permission for a specific feature to behave as expected. In this UI,
+                // include a "cancel" or "no thanks" button that allows the user to
+                // continue using your app without granting the permission.
+    //            showInContextUI(...)
+            }
+                else -> {
+                    // You can directly ask for the permission.
+                    // The registered ActivityResultCallback gets the result of this request.
+                    requestPermissionLauncher.launch(
+                        Manifest.permission.READ_EXTERNAL_STORAGE)
+                }
+            }
+        }
+
+    }
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                // Permission is granted. Continue the action or workflow in your
+                // app.
+            } else {
+                // Explain to the user that the feature is unavailable because the
+                // features requires a permission that the user has denied. At the
+                // same time, respect the user's decision. Don't link to system
+                // settings in an effort to convince the user to change their
+                // decision.
+            }
+        }
+
 }
