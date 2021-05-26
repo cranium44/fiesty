dc21948b032c842c16c80cb1c181f693303d3475
Adabe Echaim <adabeechaim@gmail.com>
2021-05-26 16:36:47 +0100

    :recycle: Refactor code

--- app/src/main/java/live/adabe/fiesty/ui/profile/ProfileFragment.kt
+++ app/src/main/java/live/adabe/fiesty/ui/profile/ProfileFragment.kt
@@ -6,15 +6,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import dagger.hilt.android.AndroidEntryPoint
 import live.adabe.fiesty.R
+import live.adabe.fiesty.databinding.ProfileFragmentBinding
 
+@AndroidEntryPoint
 class ProfileFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = ProfileFragment()
-    }
-
     private lateinit var viewModel: ProfileViewModel
+    private lateinit var binding: ProfileFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
